0be6bdfc7a66e1480cf4956eee778931999707f2
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-07 22:12:19 +0200

    [UPD] stilizzati grid e elementSquare

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@
 function createSquare() {
     let elementSquare = document.createElement('div');
 
+    elementSquare.classList.add('square');
+
     return elementSquare;
 }
 
 // richiamo l'elemento #grid dal dom
 const grid = document.getElementById('grid');
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
     let square = createSquare();
 
-    square.innerText = i + 1;
+    square.innerText = i;
 
     // appendo la casella all'elemento #grid
     grid.append(square);
     
 }
-
-
-
-
